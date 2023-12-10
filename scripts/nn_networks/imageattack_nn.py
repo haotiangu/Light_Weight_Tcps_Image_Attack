5ca3e2543e91463e976b4a48070848ddc507ca58
Haotian Gu <47264363+haotiangu@users.noreply.github.com>
2023-12-09 20:43:28 -0500

    Update imageattack_nn.py

--- scripts/nn_networks/imageattack_nn.py
+++ scripts/nn_networks/imageattack_nn.py
@@ -1,95 +1,234 @@
-#!/usr/bin/env python3
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-class ImageAttackNetwork(nn.Module):
 
-    def __init__(self, h, w, action_dim):
-        super(ImageAttackNetwork, self).__init__()
+EPS = 0.003
 
-        ngf = 16
-        nc = 3
+def fanin_init(size, fanin=None):
+	fanin = fanin or size[0]
+	v = 1. / np.sqrt(fanin)
+	return torch.Tensor(size).uniform_(-v, v)
+    
+class Critic(nn.Module):
 
-        self.encoding_dim = 64
-        self.height = h
-        self.width = w
-        self.action_dim = action_dim
+	def __init__(self, state_dim, action_dim):
+		"""
+		:param state_dim: Dimension of input state (int)
+		:param action_dim: Dimension of input action (int)
+		:return:
+		"""
+		super(Critic, self).__init__()
 
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, 32, 12, stride=5), nn.BatchNorm2d(32), nn.ReLU(),
-            nn.Conv2d(32, 64, 8, stride=4), nn.BatchNorm2d(64), nn.ReLU(),
-            nn.Conv2d(64, 32, 4, stride=2), nn.BatchNorm2d(32), nn.ReLU(),
-            nn.Conv2d(32, 16, 3, stride=1), nn.BatchNorm2d(16), nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(784, self.encoding_dim)  #<--- 784 is hard-coded as dependent on 448 x 448 x 3.
-        )
+		self.state_dim = state_dim
+		self.action_dim = action_dim
 
-        # self.decoder = nn.Sequential(
-        #     # input is Z, going into a convolutionc
-        #     nn.ConvTranspose2d( self.encoding_dim, ngf * 8, 4, 1, 0, bias=False),
-        #     nn.BatchNorm2d(ngf * 8), nn.ReLU(True),
-        #     nn.ConvTranspose2d(ngf * 8, ngf * 8, 5, 2, 1, bias=False),
-        #     nn.BatchNorm2d(ngf * 8), nn.ReLU(True),
-        #     nn.ConvTranspose2d(ngf * 8, ngf * 4, 5, 2, 1, bias=False),
-        #     nn.BatchNorm2d(ngf * 4), nn.ReLU(True),
-        #     nn.ConvTranspose2d( ngf * 4, ngf * 4, 4, 2, 1, bias=False),
-        #     nn.BatchNorm2d(ngf * 4), nn.ReLU(True),
-        #     nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-        #     nn.BatchNorm2d(ngf * 2), nn.ReLU(True),
-        #     nn.ConvTranspose2d( ngf * 2, ngf, 7, 3, 1, bias=False),
-        #     nn.BatchNorm2d(ngf), nn.ReLU(True),
-        #     nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
-        #     nn.Tanh()
-        # )
+		self.fcs1 = nn.Linear(state_dim,256)
+		#self.fcs1.weight.data = fanin_init(self.fcs1.weight.data.size())
+		self.fcs2 = nn.Linear(256,128)
+		#self.fcs2.weight.data = fanin_init(self.fcs2.weight.data.size())
 
-        self.attack_generator = nn.Sequential(
-            # input is Z, going into a convolutionc
-            nn.ConvTranspose2d( self.encoding_dim + self.action_dim, ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 8), nn.ReLU(True),
-            nn.ConvTranspose2d(ngf * 8, ngf * 8, 5, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 8), nn.ReLU(True),
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 5, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4), nn.ReLU(True),
-            nn.ConvTranspose2d( ngf * 4, ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4), nn.ReLU(True),
-            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2), nn.ReLU(True),
-            nn.ConvTranspose2d( ngf * 2, ngf, 7, 3, 1, bias=False),
-            nn.BatchNorm2d(ngf), nn.ReLU(True),
-            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
-            nn.Tanh()
-        )
+		self.fca1 = nn.Linear(action_dim,128)
+		#self.fca1.weight.data = fanin_init(self.fca1.weight.data.size())
 
-    # def reconstruct_image(self, x_img):
-    #     encoding = self.encoder(x_img)
-    #     x = torch.unsqueeze(torch.unsqueeze(encoding, -1), -1)
-    #     x = self.decoder(x)
-    #     x = x[:, :, :self.width, :self.height] # Crop Image
-    #     return x
+		self.fc2 = nn.Linear(256,128)
+		#self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
 
-    def get_attack_image(self, x_img, tgt):
-        encoding = self.encoder(x_img)
-        x = torch.cat([encoding, tgt], 1)
-        x = torch.unsqueeze(torch.unsqueeze(x, -1), -1)
-        x = self.attack_generator(x)
-        x = x[:, :, :self.width, :self.height] # Crop Image
-        return x
+		self.fc3 = nn.Linear(128,1)
+		self.fc3.weight.data.uniform_(-EPS,EPS)
+
+	def forward(self, state, action):
+		"""
+		returns Value function Q(s,a) obtained from critic network
+		:param state: Input state (Torch Variable : [n,state_dim] )
+		:param action: Input Action (Torch Variable : [n,action_dim] )
+		:return: Value function : Q(S,a) (Torch Variable : [n,1] )
+		"""
+		s1 = F.relu(self.fcs1(state))
+		s2 = F.relu(self.fcs2(s1))
+		a1 = F.relu(self.fca1(action))
+		x = torch.cat((s2,a1),dim=1)
+
+		x = F.relu(self.fc2(x))
+		x = self.fc3(x)
+
+		return x
 
 
 
+class Actor(nn.Module):
+
+	def __init__(self, state_dim, action_dim, action_lim):
+		"""
+		:param state_dim: Dimension of input state (int)
+		:param action_dim: Dimension of output action (int)
+		:param action_lim: Used to limit action in [-action_lim,action_lim]
+		:return:
+		"""
+		super(Actor, self).__init__()
+
+		self.state_dim = state_dim
+		self.action_dim = action_dim
+		self.action_lim = action_lim
+
+		self.fc1 = nn.Linear(state_dim,256)
+		#self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+
+		self.fc2 = nn.Linear(256,128)
+		#self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+
+		self.fc3 = nn.Linear(128,64)
+		#self.fc3.weight.data = fanin_init(self.fc3.weight.data.size())
+
+		self.fc4 = nn.Linear(64,action_dim)
+		self.fc4.weight.data.uniform_(-EPS,EPS)
+
+	def forward(self, state):
+		"""
+		returns policy function Pi(s) obtained from actor network
+		this function is a gaussian prob distribution for all actions
+		with mean lying in (-1,1) and sigma lying in (0,1)
+		The sampled action can , then later be rescaled
+		:param state: Input state (Torch Variable : [n,state_dim] )
+		:return: Output action (Torch Variable: [n,action_dim] )
+		"""
+		x = F.relu(self.fc1(state))
+		x = F.relu(self.fc2(x))
+		x = F.relu(self.fc3(x))
+		action = torch.tanh(self.fc4(x))
+
+		action = action * self.action_lim
+
+		return action
+
+
+class SmallImageCritic(nn.Module):
+
+	def __init__(self, image_size, state_dim, action_dim):
+		"""
+		:param state_dim: Dimension of input state (int)
+		:param action_dim: Dimension of input action (int)
+		:return:
+		"""
+		super(SmallImageCritic, self).__init__()
+
+		self.state_dim = state_dim
+		self.action_dim = action_dim
+
+		### Image Encoder ###
+		self.encoder = nn.Sequential(
+			nn.Conv2d(3, 32, 12, stride=4), nn.BatchNorm2d(32), nn.ReLU(),
+			nn.Conv2d(32, 64, 8, stride=2), nn.BatchNorm2d(64), nn.ReLU(),
+			#nn.Conv2d(64, 32, 4, stride=1), nn.BatchNorm2d(32), nn.ReLU(),
+			#nn.Conv2d(32, 16, 3, stride=1), nn.BatchNorm2d(16), nn.ReLU(),
+			nn.Flatten(), #,
+			nn.Linear(6400, self.state_dim)  #<--- 784 is hard-coded as dependent on 448 x 448 x 3.    16 is hard-coded as dependent on 224 x 224 x 3.
+		)
+
+		self.fcs1 = nn.Linear(state_dim,256)
+		#self.fcs1.weight.data = fanin_init(self.fcs1.weight.data.size())
+		self.fcs2 = nn.Linear(256,128)
+		#self.fcs2.weight.data = fanin_init(self.fcs2.weight.data.size())
+
+		self.fca1 = nn.Linear(action_dim,128)
+		#self.fca1.weight.data = fanin_init(self.fca1.weight.data.size())
+
+		self.fc2 = nn.Linear(256,128)
+		#self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+
+		self.fc3 = nn.Linear(128,1)
+		self.fc3.weight.data.uniform_(-EPS,EPS)
+
+	def forward(self, image, action):
+		"""
+		returns Value function Q(s,a) obtained from critic network
+		:param state: Input state (Torch Variable : [n,state_dim] )
+		:param action: Input Action (Torch Variable : [n,action_dim] )
+		:return: Value function : Q(S,a) (Torch Variable : [n,1] )
+		"""
+
+		state = self.encoder(image)
+
+		s1 = F.relu(self.fcs1(state))
+		s2 = F.relu(self.fcs2(s1))
+		a1 = F.relu(self.fca1(action))
+		x = torch.cat((s2,a1),dim=1)
+
+		x = F.relu(self.fc2(x))
+		x = self.fc3(x)
+
+		return x
+
+
+
+
+class SmallImageActor(nn.Module):
+
+	def __init__(self, image_size, state_dim, action_dim, action_lim):
+		"""
+		:param state_dim: Dimension of input state (int)
+		:param action_dim: Dimension of output action (int)
+		:param action_lim: Used to limit action in [-action_lim,action_lim]
+		:return:
+		"""
+		super(SmallImageActor, self).__init__()
+
+		self.state_dim = state_dim
+		self.action_dim = action_dim
+		self.action_lim = action_lim
+
+		### Image Encoder ###
+		self.encoder = nn.Sequential(
+			nn.Conv2d(3, 32, 12, stride=4), nn.BatchNorm2d(32), nn.ReLU(),
+			nn.Conv2d(32, 64, 8, stride=2), nn.BatchNorm2d(64), nn.ReLU(),
+			#nn.Conv2d(64, 32, 4, stride=1), nn.BatchNorm2d(32), nn.ReLU(),
+			#nn.Conv2d(32, 16, 3, stride=1), nn.BatchNorm2d(16), nn.ReLU(),
+			nn.Flatten(), #,
+			nn.Linear(6400, self.state_dim)  #<--- 784 is hard-coded as dependent on 448 x 448 x 3.    16 is hard-coded as dependent on 224 x 224 x 3.
+		)
+
+		self.fc1 = nn.Linear(state_dim,256)
+		#self.fc1.weight.data = fanin_init(self.fc1.weight.data.size())
+
+		self.fc2 = nn.Linear(256,128)
+		#self.fc2.weight.data = fanin_init(self.fc2.weight.data.size())
+
+		self.fc3 = nn.Linear(128,64)
+		#self.fc3.weight.data = fanin_init(self.fc3.weight.data.size())
+
+		self.fc4 = nn.Linear(64,action_dim)
+		self.fc4.weight.data.uniform_(-EPS,EPS)
+
+	def forward(self, image):
+		"""
+		returns policy function Pi(s) obtained from actor network
+		this function is a gaussian prob distribution for all actions
+		with mean lying in (-1,1) and sigma lying in (0,1)
+		The sampled action can , then later be rescaled
+		:param state: Input state (Torch Variable : [n,state_dim] )
+		:return: Output action (Torch Variable: [n,action_dim] )
+		"""
+
+		state = self.encoder(image)
+
+		x = F.relu(self.fc1(state))
+		x = F.relu(self.fc2(x))
+		x = F.relu(self.fc3(x))
+		action = torch.tanh(self.fc4(x))
+
+		action = action * self.action_lim
+
+		return action
 
 
 if __name__ == "__main__":
-    DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    generator = ImageAttackNetwork(h=448, w=448, action_dim=3).to(DEVICE)
 
-    x = torch.FloatTensor(np.random.rand(1, 3, 448, 448)).to(DEVICE)
-    print('x', x.size())
+	actor = SmallImageActor(image_size=(64,64), state_dim=32, action_dim=4, action_lim=1)
+	x = torch.FloatTensor(np.random.rand(2, 3, 112, 112))
+	action = actor.forward(x)
+	print(action)
 
-    act = torch.FloatTensor(np.random.rand(1,3)).to(DEVICE)
-    print('act', act.size())
-
-    img = generator.get_attack_image(x, act)
-    print('img', img.size())
+	critic = SmallImageCritic(image_size=(64, 64), state_dim=32, action_dim=4)
+	values = critic.forward(x, action)
+	print(values)
